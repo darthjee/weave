cae59acaee5ffd71c277d4a206f864441929009b
darthjee <darthjee@gmail.com>
2025-09-10 17:28:49 -0300

    Extract functions

--- source/bin/configure_database.sh
+++ source/bin/configure_database.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+create() {
+  mysql -h "$WEAVE_MYSQL_HOST" -u "$WEAVE_MYSQL_USER" -p"$WEAVE_MYSQL_PASSWORD" < "mysql/create_dev_database.sql"
+}
+
+migrate() {
+  python manage.py migrate
+}
+
 case "$1" in
   create)
-    mysql -h "$WEAVE_MYSQL_HOST" -u "$WEAVE_MYSQL_USER" -p"$WEAVE_MYSQL_PASSWORD" < "mysql/create_dev_database.sql"
+    create
     ;;
   migrate)
-    python manage.py migrate
+    migrate
     ;;
   *)
     echo "Usage: $0 {create|migrate}"
