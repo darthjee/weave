af54e4aa7a868ed1b29905188f3742076d647b2d
darthjee <darthjee@gmail.com>
2025-09-10 22:14:24 -0300

    Add render scripts

---
+++ scripts/prod_shell.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+source "scripts/render.sh"
+
+IMAGE="$RENDER_SERVICE_NAME"_production
+
+function run() {
+  SERVICE_ID=$(service_id)
+
+  setup_env $SERVICE_ID
+  clean_env & run_docker
+}
+
+function run_docker() {
+  docker-compose run $IMAGE /bin/bash
+}
+
+function up() {
+  SERVICE_ID=$(service_id)
+
+  setup_env $SERVICE_ID
+  clean_env & up_docker
+}
+
+function up_docker() {
+  docker-compose up $IMAGE
+}
+
+function clean_env() {
+  sleep 10
+  echo "" > .env.production
+}
+
+function setup_env() {
+  get_env_vars $1 | \
+    jq 'map([.key, .value] | join("=")) | .[]' | \
+    sed -e 's/^ *"//g' -e 's/" *$//g'  > .env.production
+}
+
+ACTION=$1
+
+case $ACTION in
+  "run")
+    run
+    ;;
+  "up")
+    up
+    ;;
+  *)
+    $ACTION
+    ;;
+esac
