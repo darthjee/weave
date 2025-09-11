9e6fc4eeeff24cb8193b9a86cef4ab22065d334c
darthjee <darthjee@gmail.com>
2025-09-10 21:36:32 -0300

    Add stage flip

--- source/bin/server.sh
+++ source/bin/server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python manage.py runserver 0.0.0.0:8080
+
+if [ "$STAGE" = "production" ]; then
+  gunicorn weave.wsgi:application --bind 0.0.0.0:8080
+else
+  python manage.py runserver 0.0.0.0:8080
+fi
