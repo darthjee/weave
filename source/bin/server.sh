9c1f62e57fdef78434b2450a51fc7414a50c6dc6
darthjee <darthjee@gmail.com>
2025-09-14 11:51:29 -0300

    ENsure migration is run

--- source/bin/server.sh
+++ source/bin/server.sh
@@ -3,5 +3,6 @@
 if [ "$STAGE" = "production" ]; then
   poetry run gunicorn weave.wsgi:application --bind 0.0.0.0:8080
 else
+  bin/configure_database.sh all
   poetry run python manage.py runserver 0.0.0.0:8080
 fi
