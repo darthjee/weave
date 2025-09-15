c3942862084264d8585eec379fc7ccb3ffe2e834
darthjee <darthjee@gmail.com>
2025-09-15 16:59:30 -0300

    Build assets

--- source/bin/deploy.sh
+++ source/bin/deploy.sh
@@ -4,18 +4,26 @@ function migrate() {
   ./bin/configure_database.sh migrate
 }
 
+function build() {
+  poetry run python manage.py collectstatic --noinput
+}
+
 function all() {
   migrate
+  build
 }
 
 case "$1" in
   migrate)
     migrate
     ;;
+  build)
+    build
+    ;;
   all)
     all
     ;;
   *)
-    echo "Usage: $0 {migrate|all}"
+    echo "Usage: $0 {migrate|build|all}"
     ;;
 esac
