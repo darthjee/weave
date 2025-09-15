ee94325409d228efdb0d0ae41d153c1e880650b8
darthjee <darthjee@gmail.com>
2025-09-15 18:51:05 -0300

    Add keys

--- source/bin/deploy.sh
+++ source/bin/deploy.sh
@@ -11,7 +11,15 @@ function build() {
 upload() {
   LOCAL_DIR="static"
 
-  rsync -avz -e "ssh -p $SSH_PORT" static/ $SSH_USER@$SSH_HOST:$SSH_REMOTE_DIR
+  # Cria arquivo temporário para a chave privada
+  SSH_KEY_FILE=$(mktemp)
+  echo "$SSH_PRIVATE_KEY" > "$SSH_KEY_FILE"
+  chmod 600 "$SSH_KEY_FILE"
+
+  rsync -avz -e "ssh -p $SSH_PORT -i $SSH_KEY_FILE" static/ $SSH_USER@$SSH_HOST:$SSH_REMOTE_DIR
+
+  # Remove arquivo temporário
+  rm -f "$SSH_KEY_FILE"
 }
 
 function all() {
