2eea1bfa4d1c158d6af77a86444003163eedd5a7
darthjee <darthjee@gmail.com>
2025-09-15 10:01:02 -0300

    Move home to home app

--- source/weave/urls.py
+++ source/weave/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, "home.html")
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('home.urls')),
 ]
