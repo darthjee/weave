4592b2f3920d65ac2c4d0c33e52df97242e023b9
darthjee <darthjee@gmail.com>
2025-09-15 16:22:25 -0300

    Run production normally

--- source/bin/server.sh
+++ source/bin/server.sh
@@ -4,7 +4,7 @@ if [ "$CREATE_DB" = "true" ]; then
   bin/configure_database.sh all
 fi
 
-if [ "$STAGE" = "production" ] && [ "$SERVE_STATIC" != "true" ]; then
+if [ "$STAGE" = "production" ]; then
   poetry run gunicorn weave.wsgi:application --bind 0.0.0.0:8080
 else
   poetry run python manage.py runserver 0.0.0.0:8080
