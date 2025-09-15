bf24420a2a6ca2466672ae2d543415574f7177b6
darthjee <darthjee@gmail.com>
2025-09-15 10:25:20 -0300

    Configure static file

--- source/weave/urls.py
+++ source/weave/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('', include('home.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
