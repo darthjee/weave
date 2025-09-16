a6da64d6c97bfcab54e0926588de18112523d231
darthjee <darthjee@gmail.com>
2025-09-16 13:19:10 -0300

    Refactor script

--- source/bin/deploy.sh
+++ source/bin/deploy.sh
@@ -13,14 +13,8 @@ upload() {
 
   SSH_KEY_FILE=$(createSshKeyFile)
 
-  SSH_REMOTE_TEMP_DIR="${SSH_REMOTE_DIR}_$RANDOM"
-  SSH_REMOTE_OLD_DIR="${SSH_REMOTE_DIR}_old"
-
-  ssh -i "$SSH_KEY_FILE" -p "$SSH_PORT" -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "mkdir -p $SSH_REMOTE_DIR"
-  ssh -i "$SSH_KEY_FILE" -p "$SSH_PORT" -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "mkdir -p $SSH_REMOTE_TEMP_DIR"
-  scp -i "$SSH_KEY_FILE" -P "$SSH_PORT" -o StrictHostKeyChecking=no -r static/* $SSH_USER@$SSH_HOST:$SSH_REMOTE_TEMP_DIR
-  ssh -i "$SSH_KEY_FILE" -p "$SSH_PORT" -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "mv $SSH_REMOTE_DIR $SSH_REMOTE_OLD_DIR; mv $SSH_REMOTE_TEMP_DIR $SSH_REMOTE_DIR; rm -rf $SSH_REMOTE_OLD_DIR"
-
+  uploadFiles
+  
   rm -f "$SSH_KEY_FILE"
 }
 
@@ -30,6 +24,17 @@ function createSshKeyFile() {
   chmod 600 "$SSH_KEY_FILE"
   echo $SSH_KEY_FILE
 }
+
+function uploadFiles() {
+  SSH_REMOTE_TEMP_DIR="${SSH_REMOTE_DIR}_$RANDOM"
+  SSH_REMOTE_OLD_DIR="${SSH_REMOTE_DIR}_old"
+
+  ssh -i "$SSH_KEY_FILE" -p "$SSH_PORT" -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "mkdir -p $SSH_REMOTE_DIR"
+  ssh -i "$SSH_KEY_FILE" -p "$SSH_PORT" -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "mkdir -p $SSH_REMOTE_TEMP_DIR"
+  scp -i "$SSH_KEY_FILE" -P "$SSH_PORT" -o StrictHostKeyChecking=no -r static/* $SSH_USER@$SSH_HOST:$SSH_REMOTE_TEMP_DIR
+  ssh -i "$SSH_KEY_FILE" -p "$SSH_PORT" -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "mv $SSH_REMOTE_DIR $SSH_REMOTE_OLD_DIR; mv $SSH_REMOTE_TEMP_DIR $SSH_REMOTE_DIR; rm -rf $SSH_REMOTE_OLD_DIR"
+}
+
 
 function all() {
   migrate
