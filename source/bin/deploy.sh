9324e3511e412a05d30053e1460908e10d74f413
Fernando Favini <darthjee@gmail.com>
2025-09-17 09:31:51 -0300

    Fix buid static files step (#22)

--- source/bin/deploy.sh
+++ source/bin/deploy.sh
@@ -20,7 +20,7 @@ upload() {
 
 function createSshKeyFile() {
   SSH_KEY_FILE=$(mktemp)
-  echo "$SSH_PRIVATE_KEY" > "$SSH_KEY_FILE"
+  echo "$SSH_PRIVATE_KEY" | sed -e "s/\\\n/\\n/g" > "$SSH_KEY_FILE"
   chmod 600 "$SSH_KEY_FILE"
   echo $SSH_KEY_FILE
 }
@@ -35,15 +35,50 @@ function uploadFiles() {
   ssh -i "$SSH_KEY_FILE" -p "$SSH_PORT" -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "mv $SSH_REMOTE_DIR $SSH_REMOTE_OLD_DIR; mv $SSH_REMOTE_TEMP_DIR $SSH_REMOTE_DIR; rm -rf $SSH_REMOTE_OLD_DIR"
 }
 
+function isLatestCommit() {
+  VERSION=$(git tag | grep $(git describe  --tags))
+
+  if [[ $VERSION ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function checkLastVersion() {
+  if $(isLatestCommit); then
+    echo "latest commit";
+  else
+    echo "Not last commit"
+    exit 0
+  fi
+}
 
 function all() {
+  checkLastVersion
   migrate
+  force_build_and_upload
+}
+
+function build_and_upload() {
+  checkLastVersion
+  force_build_and_upload
+}
+
+function force_build_and_upload() {
   build
+  upload
 }
 
 case "$1" in
   migrate)
     migrate
+    ;;
+  build_and_upload)
+    build_and_upload
+    ;;
+  force_build_and_upload)
+    force_build_and_upload
     ;;
   build)
     build
@@ -55,6 +90,6 @@ case "$1" in
     all
     ;;
   *)
-    echo "Usage: $0 {migrate|build|upload|all}"
+    echo "Usage: $0 {migrate|build|upload|all|build_and_upload|force_build_and_upload}"
     ;;
 esac
