5899fa07d6c05d66084414fac4696de849abe137
darthjee <darthjee@gmail.com>
2025-09-18 21:02:16 -0300

    Skip manage.py o report

--- source/bin/reports.sh
+++ source/bin/reports.sh
@@ -3,19 +3,20 @@ set -e
 
 # Pasta do código
 CODE_DIR="."
+EXCLUDE="manage.py"
 
 case "$1" in
   complexity)
-    poetry run radon cc $CODE_DIR -s -a
+    poetry run radon cc $CODE_DIR -s -a --exclude $EXCLUDE
     ;;
   maintainability)
-    poetry run radon mi $CODE_DIR
+    poetry run radon mi $CODE_DIR --exclude $EXCLUDE
     ;;
   ci)
-    poetry run xenon $CODE_DIR --max-absolute B --max-modules A --max-average A
+    poetry run xenon $CODE_DIR --max-absolute B --max-modules A --max-average A --exclude $EXCLUDE
     ;;
   lizard)
-    poetry run lizard --html -o lizard_report.html $CODE_DIR
+    poetry run lizard -x $EXCLUDE --html -o lizard_report.html $CODE_DIR
     echo "Lizard HTML report generated: lizard_report.html"
     ;;
   *)
