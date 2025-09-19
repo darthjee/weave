5e1804869d1fafdafcedacabb22cd0e745bc67ea
Fernando Favini <darthjee@gmail.com>
2025-09-18 22:01:03 -0300

    Complexity checks (#28)

---
+++ source/bin/reports.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Pasta do código
+CODE_DIR="."
+EXCLUDE="manage.py,node_modules,static"
+
+case "$1" in
+  complexity)
+    poetry run radon cc $CODE_DIR -s -a --exclude $EXCLUDE
+    ;;
+  maintainability)
+    poetry run radon mi $CODE_DIR --exclude $EXCLUDE
+    ;;
+  ci)
+    poetry run xenon $CODE_DIR --max-absolute B --max-modules A --max-average A --exclude $EXCLUDE
+    ;;
+  lizard)
+    poetry run lizard \
+      -x "./manage.py" \
+      -x "./node_modules/*" \
+      -x "./static/*" \
+      -H -o lizard_report.html $CODE_DIR
+    echo "Lizard HTML report generated: lizard_report.html"
+    ;;
+  *)
+    echo "Usage: $0 {complexity|maintainability|ci|lizard}"
+    exit 1
+    ;;
+esac
+
