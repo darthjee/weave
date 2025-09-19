8458964a62c2d79c99ac37599e19931bb97a1ca8
darthjee <darthjee@gmail.com>
2025-09-18 21:06:57 -0300

    Ignore more folders

--- source/bin/reports.sh
+++ source/bin/reports.sh
@@ -3,7 +3,7 @@ set -e
 
 # Pasta do código
 CODE_DIR="."
-EXCLUDE="manage.py"
+EXCLUDE="manage.py,node_modules,static"
 
 case "$1" in
   complexity)
@@ -16,7 +16,7 @@ case "$1" in
     poetry run xenon $CODE_DIR --max-absolute B --max-modules A --max-average A --exclude $EXCLUDE
     ;;
   lizard)
-    poetry run lizard -x $EXCLUDE --html -o lizard_report.html $CODE_DIR
+    poetry run lizard -x manage.py -x node_modules -x static -H -o lizard_report.html $CODE_DIR
     echo "Lizard HTML report generated: lizard_report.html"
     ;;
   *)
