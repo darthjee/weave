b0f911346efc546882743c6def86c238d9e952c5
darthjee <darthjee@gmail.com>
2025-12-16 18:42:07 -0300

    Add react files

---
+++ source/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Weave React App</h1>
+      <p>This is a minimal React app served by Django static files.</p>
+    </div>
+  )
+}
