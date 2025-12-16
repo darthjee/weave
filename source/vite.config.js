823e5cb7b45390d88a46bac1e1fd7418aee654c3
darthjee <darthjee@gmail.com>
2025-12-16 19:14:48 -0300

    Compile js

--- source/vite.config.js
+++ source/vite.config.js
@@ -2,24 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // project root that contains source files
+  // project root that contains index.html
   root: './',
   plugins: [react()],
   build: {
     // outDir is relative to the root option
     outDir: 'weave/static',
     emptyOutDir: true,
-    // library mode: produce only JS bundle (no index.html)
-    lib: {
-      entry: 'src/main.jsx',
-      name: 'WeaveApp',
-      formats: ['es'],
-      fileName: () => 'js/app.js'
-    },
     rollupOptions: {
-      // bundle React into the build by default (remove from external)
-      // control asset file names (CSS/images)
+      input: 'src/main.jsx',
       output: {
+        entryFileNames: 'js/[name].js',
+        chunkFileNames: 'js/[name].js',
         assetFileNames: ({ name }) => {
           if (name && name.endsWith('.css')) return 'css/[name].[ext]'
           return 'assets/[name].[ext]'
