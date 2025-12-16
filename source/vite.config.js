d4cbb397d40222b59c733798745c79e00b6caee8
Fernando Favini <darthjee@gmail.com>
2025-12-16 19:46:37 -0300

    Vite setup (#31)

---
+++ source/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  // project root that contains index.html
+  root: './',
+  plugins: [react()],
+  build: {
+    // outDir is relative to the root option
+    outDir: 'weave/static',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'src/main.jsx',
+      output: {
+        entryFileNames: 'js/[name].js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) return 'css/[name].[ext]';
+          return 'assets/[name].[ext]';
+        }
+      }
+    }
+  }
+});
