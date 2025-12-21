3ed82d1c3a07f52db366782112d622efff83901b
darthjee <darthjee@gmail.com>
2025-12-21 16:35:09 -0300

    Init box component

--- source/frontend/src/js/components/SectionBox.jsx
+++ source/frontend/src/js/components/SectionBox.jsx
@@ -2,8 +2,14 @@ import PropTypes from 'prop-types';
 
 export default function SectionBox({ children }) {
   return (
-    <div>
-      {children}
+    <div className="section-box">
+      <div className="section-box-header"></div>
+      <div className="section-box-content">
+        <div className="section-box-left-border"></div>
+        {children}
+        <div className="section-box-right-border"></div>
+      </div>
+      <div className="section-box-footer"></div>
     </div>
   );
 }
