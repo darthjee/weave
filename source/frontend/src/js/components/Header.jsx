80aed2f2c1ce176ab75dd642817b7c7c7ca2e106
darthjee <darthjee@gmail.com>
2025-12-21 16:19:14 -0300

    Add missing header component

---
+++ source/frontend/src/js/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>Weave React App Header</h1>
+      <p>This is a minimal React app served by Django static files.</p>
+    </div>
+  );
+}
