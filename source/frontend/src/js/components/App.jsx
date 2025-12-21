3ad3a32d26ca274c6dca016053ac8b0a3fa52b6d
darthjee <darthjee@gmail.com>
2025-12-21 16:18:54 -0300

    Force auto reload

--- source/frontend/src/js/components/App.jsx
+++ source/frontend/src/js/components/App.jsx
@@ -1,8 +1,7 @@
+import Header from './Header';
+
 export default function App() {
   return (
-    <div>
-      <h1>Weave React App</h1>
-      <p>This is a minimal React app served by Django static files.</p>
-    </div>
+    <Header />
   );
 }
