59054d212789fca796cc42b42a1897d5ebffcbe6
darthjee <darthjee@gmail.com>
2025-12-21 16:26:23 -0300

    Allow box children

--- source/frontend/src/js/components/Header.jsx
+++ source/frontend/src/js/components/Header.jsx
@@ -1,8 +1,10 @@
+import Box from './Box';
+
 export default function Header() {
   return (
-    <div>
+    <Box>
       <h1>Weave React App Header</h1>
       <p>This is a minimal React app served by Django static files.</p>
-    </div>
+    </Box>
   );
 }
