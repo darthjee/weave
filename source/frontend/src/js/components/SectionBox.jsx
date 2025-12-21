4b67c5d0ec4492e7e677661a2b4fcd63f5d6e9ba
darthjee <darthjee@gmail.com>
2025-12-21 19:23:25 -0300

    Use flex

--- source/frontend/src/js/components/SectionBox.jsx
+++ source/frontend/src/js/components/SectionBox.jsx
@@ -4,11 +4,11 @@ export default function SectionBox({ children }) {
   return (
     <div className="section-box">
       <div className="section-box-header"></div>
+      <div className="section-box-left-border"></div>
       <div className="section-box-content">
-        <div className="section-box-left-border"></div>
         {children}
-        <div className="section-box-right-border"></div>
       </div>
+      <div className="section-box-right-border"></div>
       <div className="section-box-footer"></div>
     </div>
   );
