7f266587726f2e44362b97a14ec55218c7755d57
darthjee <darthjee@gmail.com>
2025-12-25 16:09:27 -0300

    Use 3000 as FE

--- backend/weave/urls.py
+++ backend/weave/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('ok')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', index, name='index'),
 ]
