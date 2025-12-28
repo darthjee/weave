9ca92e776d7679328a276c266282d5d4a7f8b19d
darthjee <darthjee@gmail.com>
2025-12-27 21:40:22 -0300

    Ignore files from lint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "eslint/config";
 import complexity from "eslint-plugin-complexity";
 
-export default defineConfig([{
+export default defineConfig([
+{
+    ignores: [
+        'node_modules/**/*.js',
+        'dist/**/*.js',
+        'report/**'
+    ]
+},
+{
     plugins: {
         complexity,
     },
