59d9ebf5e415b8e571fbaa740be4f6abcbbc1e7e
darthjee <darthjee@gmail.com>
2025-12-29 22:22:51 -0300

    Use bootstrap container

--- frontend/assets/js/components/SectionBox.jsx
+++ frontend/assets/js/components/SectionBox.jsx
@@ -1,24 +1,34 @@
 import PropTypes from 'prop-types';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function SectionBox({ children }) {
   return (
-    <div className="section-box">
-      <div className="section-box-header">
-        <div className="section-box-top-left-corner"></div>
-        <div className="section-box-top-right-corner"></div>
-      </div>
-      <div className="section-box-middle">
-        <div className="section-box-left-border"></div>
-        <div className="section-box-content">
-          {children}
+    <Container>
+      <Row>
+        <Col>
+          <div className="section-box">
+          <div className="section-box-header">
+            <div className="section-box-top-left-corner"></div>
+            <div className="section-box-top-right-corner"></div>
+          </div>
+          <div className="section-box-middle">
+            <div className="section-box-left-border"></div>
+            <div className="section-box-content">
+              {children}
+            </div>
+            <div className="section-box-right-border"></div>
+          </div>
+          <div className="section-box-footer">
+            <div className="section-box-bottom-left-corner"></div>
+            <div className="section-box-bottom-right-corner"></div>
+          </div>
         </div>
-        <div className="section-box-right-border"></div>
-      </div>
-      <div className="section-box-footer">
-        <div className="section-box-bottom-left-corner"></div>
-        <div className="section-box-bottom-right-corner"></div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
