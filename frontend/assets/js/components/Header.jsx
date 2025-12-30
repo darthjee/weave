a14fcb45f5aaf6c899c0f33d1a25ed83ba1e0c68
Fernando Favini <darthjee@gmail.com>
2025-12-29 22:37:49 -0300

    Use bootstrap (#46)

--- frontend/assets/js/components/Header.jsx
+++ frontend/assets/js/components/Header.jsx
@@ -1,10 +1,20 @@
 import SectionBox from './SectionBox';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function Header() {
   return (
-    <SectionBox>
-      <h1>Weave React App Header</h1>
-      <p>This is a minimal React app served by Django static files.</p>
-    </SectionBox>
+    <Container fluid>
+      <Row className="justify-content-md-center">
+        <Col lg={8} md={10} sm={12}>
+          <SectionBox>
+            <h1>Weave React App Header</h1>
+            <p>This is a minimal React app served by Django static files.</p>
+          </SectionBox>
+        </Col>
+      </Row>
+    </Container>
   );
 }
--- frontend/assets/js/components/SectionBox.jsx
+++ frontend/assets/js/components/SectionBox.jsx
@@ -3,22 +3,22 @@ import PropTypes from 'prop-types';
 export default function SectionBox({ children }) {
   return (
     <div className="section-box">
-      <div className="section-box-header">
-        <div className="section-box-top-left-corner"></div>
-        <div className="section-box-top-right-corner"></div>
-      </div>
-      <div className="section-box-middle">
-        <div className="section-box-left-border"></div>
-        <div className="section-box-content">
-          {children}
-        </div>
-        <div className="section-box-right-border"></div>
-      </div>
-      <div className="section-box-footer">
-        <div className="section-box-bottom-left-corner"></div>
-        <div className="section-box-bottom-right-corner"></div>
-      </div>
+    <div className="section-box-header">
+      <div className="section-box-top-left-corner"></div>
+      <div className="section-box-top-right-corner"></div>
     </div>
+    <div className="section-box-middle">
+      <div className="section-box-left-border"></div>
+      <div className="section-box-content">
+        {children}
+      </div>
+      <div className="section-box-right-border"></div>
+    </div>
+    <div className="section-box-footer">
+      <div className="section-box-bottom-left-corner"></div>
+      <div className="section-box-bottom-right-corner"></div>
+    </div>
+  </div>
   );
 }
 
