b69121fe0a2e4dd44d5d4dd9046dc28c15278a9e
darthjee <darthjee@gmail.com>
2025-12-30 18:38:46 -0300

    Revert "Add user"
    This reverts commit a49249f8550ab10ae73e708dd0264ce531af3b0e.

--- backend/weave/settings.py
+++ backend/weave/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Local apps
-    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -137,9 +135,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Custom User model
-AUTH_USER_MODEL = 'accounts.User'
 
 # Logging configuration
 LOGGING = {
