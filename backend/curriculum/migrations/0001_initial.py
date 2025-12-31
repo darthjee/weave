15b09993ae67b6d497847376e479d2cc641df5cd
darthjee <darthjee@gmail.com>
2025-12-31 19:04:35 -0300

    Fix migration

--- backend/curriculum/migrations/0001_initial.py
+++ backend/curriculum/migrations/0001_initial.py
@@ -16,9 +16,9 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('first_name', models.CharField(max_length=100)),
+                ('middle_name', models.CharField(blank=True, max_length=100, null=True)),
                 ('last_name', models.CharField(max_length=100)),
                 ('email', models.EmailField(max_length=254, unique=True)),
-                ('phone', models.CharField(blank=True, max_length=20, null=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
             ],
