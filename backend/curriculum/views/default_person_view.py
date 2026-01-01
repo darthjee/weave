d5278d739942e4e4874f1daf9c2f30d50a795659
darthjee <darthjee@gmail.com>
2026-01-01 14:32:33 -0300

    Split classes into files

--- backend/curriculum/views/default_person_view.py
+++ backend/curriculum/views/default_person_view.py
@@ -5,16 +5,6 @@ from curriculum.models.person import Person
 from curriculum.serializers import PersonSerializer
 
 
-class PersonRetrieveView(generics.RetrieveAPIView):
-    """
-    API endpoint that retrieves a single person by ID.
-    
-    GET /api/curriculum/person/{id}/
-    """
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
-
-
 class DefaultPersonView(generics.GenericAPIView):
     """
     API endpoint that retrieves the first person.
---
+++ backend/curriculum/views/person_retrieve_view.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from curriculum.models.person import Person
+from curriculum.serializers import PersonSerializer
+
+
+class PersonRetrieveView(generics.RetrieveAPIView):
+    """
+    API endpoint that retrieves a single person by ID.
+    
+    GET /api/curriculum/person/{id}/
+    """
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
