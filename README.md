1b3eca0618d2379cc6da02baf194af9d5cb695b9
Fernando Favini <darthjee@gmail.com>
2025-12-31 22:01:42 -0300

    Person model (#47)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # weave
+
+[![Build Status](https://circleci.com/gh/darthjee/weave.svg?style=shield)](https://circleci.com/gh/darthjee/weave)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/35480a5e82e74ff7a0186697b3f61a4b)](https://app.codacy.com/gh/darthjee/oak/dashboard?utm_source=gh&utm_medium=referral&utm_content=&utm_campaign=Badge_grade)
+
 An app to show curriculum as D&amp;D sheet
---
+++ backend/curriculum/models/person.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Person(models.Model):
+    """
+    Person model for Curriculum application.
+    """
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'person'
+        verbose_name = 'person'
+        verbose_name_plural = 'people'
+        ordering = ['last_name', 'first_name']
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    @property
+    def full_name(self):
+        parts = [self.first_name]
+        if self.middle_name:
+            parts.append(self.middle_name)
+        parts.append(self.last_name)
+        return ' '.join(parts)
