18521940533aeacc16c9b2760977e3e81ccd0ad2
darthjee <darthjee@gmail.com>
2026-01-02 09:16:19 -0300

    Show developer data

--- frontend/assets/js/components/App.jsx
+++ frontend/assets/js/components/App.jsx
@@ -1,7 +1,18 @@
 import Header from './Header';
 
 export default function App() {
+  // Mocked data
+  const personData = {
+    full_name: "Developer Name",
+    years_of_experience: Math.floor(Math.random() * 50) + 1,
+    roles: ['backend', 'frontend']
+  };
+
   return (
-    <Header />
+    <Header 
+      full_name={personData.full_name}
+      years_of_experience={personData.years_of_experience}
+      roles={personData.roles}
+    />
   );
 }
