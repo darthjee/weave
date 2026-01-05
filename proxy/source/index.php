659f5b495d31d1c8106e6bcb6678717d2ff5db26
darthjee <darthjee@gmail.com>
2026-01-05 15:39:35 -0300

    Specify URLs

--- proxy/source/index.php
+++ proxy/source/index.php
@@ -49,7 +49,7 @@ $requestMethod = $request->request_method();
 
 // Check if request should be proxied to frontend
 if ($requestMethod === 'GET' && 
-    (strpos($requestUri, '/') === 0 || strpos($requestUri, '/assets/js/') === 0 || strpos($requestUri, '/assets/css/') === 0)) {
+    ($requestUri == '/' || strpos($requestUri, '/assets/js/') === 0 || strpos($requestUri, '/assets/css/') === 0 || strpos($requestUri, '/@vite/') === 0 || strpos($requestUri, '/node_modules/') === 0 || $requestUri == '/@react-refresh')) {
     
     // Proxy to frontend
     $response = proxy_request($request, 'http://frontend:8080');
