f1844b71948d4a9401f1b2088f825b806006b966
darthjee <darthjee@gmail.com>
2026-01-05 15:49:29 -0300

    Extract proxy request

--- proxy/source/index.php
+++ proxy/source/index.php
@@ -3,45 +3,7 @@
 require_once __DIR__ . '/lib/models/Response.php';
 require_once __DIR__ . '/lib/models/MissingResponse.php';
 require_once __DIR__ . '/lib/models/Request.php';
-
-function proxy_request($request, $targetHost) {
-    // Build full URL from target host and request path
-    $url = $targetHost . $request->request_url();
-    if ($request->query()) {
-        $url .= '?' . $request->query();
-    }
-    
-    // Get all request headers
-    $requestHeaders = $request->headers();
-    $headers = [];
-    foreach ($requestHeaders as $name => $value) {
-        $headers[] = "$name: $value";
-    }
-    
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    
-    $response = curl_exec($ch);
-    $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    
-    $headers = substr($response, 0, $headerSize);
-    $body = substr($response, $headerSize);
-    
-    curl_close($ch);
-    
-    // Parse headers into array
-    $headerLines = explode("\n", $headers);
-    $headerLines = array_map('trim', $headerLines);
-    $headerLines = array_filter($headerLines, function($header) {
-        return !empty($header) && strpos($header, 'HTTP/') !== 0;
-    });
-    
-    return new Response($body, $httpCode, $headerLines);
-}
+require_once __DIR__ . '/lib/models/ProxyRequest.php';
 
 function send_response($response) {
     http_response_code($response->httpCode);
@@ -61,7 +23,8 @@ if ($requestMethod === 'GET' &&
     ($requestUri == '/' || strpos($requestUri, '/assets/js/') === 0 || strpos($requestUri, '/assets/css/') === 0 || strpos($requestUri, '/@vite/') === 0 || strpos($requestUri, '/node_modules/') === 0 || $requestUri == '/@react-refresh')) {
     
     // Proxy to frontend
-    $response = proxy_request($request, 'http://frontend:8080');
+    $proxyRequest = new ProxyRequest('http://frontend:8080');
+    $response = $proxyRequest->proxy_request($request);
 } else {
     $response = new MissingResponse();
 }
