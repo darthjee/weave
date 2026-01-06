37f50b18170f9663886a9ee1d9a675355dfef745
Fernando Favini <darthjee@gmail.com>
2026-01-06 15:54:06 -0300

    PHP unit tests (#66)

--- proxy/source/lib/models/RequestMatcher.php
+++ proxy/source/lib/models/RequestMatcher.php
@@ -5,15 +5,23 @@ class RequestMatcher {
     private $requestUri;
     private $matchType;
 
-    public function __construct($requestMethod, $requestUri, $matchType = 'exact') {
+    public function __construct($requestMethod = NULL, $requestUri = NULL, $matchType = 'exact') {
         $this->requestMethod = $requestMethod;
         $this->requestUri = $requestUri;
         $this->matchType = $matchType;
     }
 
     public function matches($request) {
-        if ($request->request_method() !== $this->requestMethod) {
-            return false;
+        return $this->matchRequestMethod($request) && $this->matchRequestUri($request);
+    }
+
+    private function matchRequestMethod($request) {
+        return $this->requestMethod == null || $request->request_method() == $this->requestMethod;
+    }
+
+    private function matchRequestUri($request) {
+        if ($this->requestUri == null) {
+            return true;
         }
 
         $requestUrl = $request->request_url();
