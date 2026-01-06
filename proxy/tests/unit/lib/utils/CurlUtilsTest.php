b3bc64626b9c2595323b227c9ef647ec941cf311
darthjee <darthjee@gmail.com>
2026-01-06 20:32:40 -0300

    Add test for parseResponseHeaders

--- proxy/tests/unit/lib/utils/CurlUtilsTest.php
+++ proxy/tests/unit/lib/utils/CurlUtilsTest.php
@@ -67,4 +67,93 @@ class CurlUtilsTest extends TestCase
 
         $this->assertEquals(['Content-Length: 1234'], $result);
     }
+
+    public function testParseResponseHeadersWithMultipleHeaders()
+    {
+        $headers = "HTTP/1.1 200 OK\nContent-Type: application/json\nContent-Length: 123\nCache-Control: no-cache";
+        $result = CurlUtils::parseResponseHeaders($headers);
+
+        $expected = [
+            1 => 'Content-Type: application/json',
+            2 => 'Content-Length: 123',
+            3 => 'Cache-Control: no-cache'
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testParseResponseHeadersRemovesHttpStatusLine()
+    {
+        $headers = "HTTP/1.1 404 Not Found\nContent-Type: text/plain";
+        $result = CurlUtils::parseResponseHeaders($headers);
+
+        $expected = [1 => 'Content-Type: text/plain'];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testParseResponseHeadersRemovesEmptyLines()
+    {
+        $headers = "HTTP/1.1 200 OK\nContent-Type: application/json\n\n\nCache-Control: no-cache\n";
+        $result = CurlUtils::parseResponseHeaders($headers);
+
+        $expected = [
+            1 => 'Content-Type: application/json',
+            4 => 'Cache-Control: no-cache'
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testParseResponseHeadersTrimsWhitespace()
+    {
+        $headers = "HTTP/1.1 200 OK\n  Content-Type: application/json  \n  Cache-Control: no-cache  ";
+        $result = CurlUtils::parseResponseHeaders($headers);
+
+        $expected = [
+            1 => 'Content-Type: application/json',
+            2 => 'Cache-Control: no-cache'
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testParseResponseHeadersWithEmptyString()
+    {
+        $headers = "";
+        $result = CurlUtils::parseResponseHeaders($headers);
+
+        $this->assertEquals([], $result);
+    }
+
+    public function testParseResponseHeadersWithOnlyHttpLine()
+    {
+        $headers = "HTTP/1.1 200 OK";
+        $result = CurlUtils::parseResponseHeaders($headers);
+
+        $this->assertEquals([], $result);
+    }
+
+    public function testParseResponseHeadersWithMultipleHttpLines()
+    {
+        $headers = "HTTP/1.1 301 Moved Permanently\nLocation: /new-url\nHTTP/1.1 200 OK\nContent-Type: text/html";
+        $result = CurlUtils::parseResponseHeaders($headers);
+
+        $expected = [
+            1 => 'Location: /new-url',
+            3 => 'Content-Type: text/html'
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testParseResponseHeadersWithSingleHeader()
+    {
+        $headers = "HTTP/1.1 200 OK\nContent-Type: application/json";
+        $result = CurlUtils::parseResponseHeaders($headers);
+
+        $expected = [1 => 'Content-Type: application/json'];
+
+        $this->assertEquals($expected, $result);
+    }
 }
