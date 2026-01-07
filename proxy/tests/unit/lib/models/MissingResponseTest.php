0117520ce48694a66ed15b9327b06de2dfc796f4
Fernando Favini <darthjee@gmail.com>
2026-01-06 21:46:52 -0300

    Increase PHP test coverage (#69)

---
+++ proxy/tests/unit/lib/models/MissingResponseTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Weave\Proxy\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Weave\Proxy\MissingResponse;
+
+require_once __DIR__ . '/../../../../source/lib/models/Response.php';
+require_once __DIR__ . '/../../../../source/lib/models/MissingResponse.php';
+
+class MissingResponseTest extends TestCase
+{
+    public function testCreatesResponseWith404StatusCode()
+    {
+        $response = new MissingResponse();
+
+        $this->assertEquals(404, $response->httpCode);
+    }
+
+    public function testCreatesResponseWithNotFoundBody()
+    {
+        $response = new MissingResponse();
+
+        $this->assertEquals("Not Found", $response->body);
+    }
+
+    public function testCreatesResponseWithTextPlainContentType()
+    {
+        $response = new MissingResponse();
+
+        $this->assertEquals(['Content-Type: text/plain'], $response->headerLines);
+    }
+
+    public function testExtendsResponse()
+    {
+        $response = new MissingResponse();
+
+        $this->assertInstanceOf(\Weave\Proxy\Response::class, $response);
+    }
+}
---
+++ proxy/tests/unit/lib/models/RequestTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Weave\Proxy\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Weave\Proxy\Request;
+
+require_once __DIR__ . '/../../../../source/lib/models/Request.php';
+
+class RequestTest extends TestCase
+{
+    private $originalServer;
+
+    protected function setUp(): void
+    {
+        // Save original values
+        $this->originalServer = $_SERVER;
+    }
+
+    protected function tearDown(): void
+    {
+        // Restore original values
+        $_SERVER = $this->originalServer;
+    }
+
+    public function testRequestMethodReturnsGetMethod()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+
+        $request = new Request();
+
+        $this->assertEquals('GET', $request->requestMethod());
+    }
+
+    public function testRequestMethodReturnsPostMethod()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+
+        $request = new Request();
+
+        $this->assertEquals('POST', $request->requestMethod());
+    }
+
+    public function testRequestUrlReturnsPath()
+    {
+        $_SERVER['REQUEST_URI'] = '/api/users';
+
+        $request = new Request();
+
+        $this->assertEquals('/api/users', $request->requestUrl());
+    }
+
+    public function testRequestUrlReturnsPathWithoutQueryString()
+    {
+        $_SERVER['REQUEST_URI'] = '/api/users?page=1&limit=10';
+
+        $request = new Request();
+
+        $this->assertEquals('/api/users', $request->requestUrl());
+    }
+
+    public function testRequestUrlReturnsRootWhenEmpty()
+    {
+        $_SERVER['REQUEST_URI'] = '/';
+
+        $request = new Request();
+
+        $this->assertEquals('/', $request->requestUrl());
+    }
+
+    public function testQueryReturnsQueryString()
+    {
+        $_SERVER['REQUEST_URI'] = '/api/users?page=1&limit=10';
+
+        $request = new Request();
+
+        $this->assertEquals('page=1&limit=10', $request->query());
+    }
+
+    public function testQueryReturnsEmptyStringWhenNoQuery()
+    {
+        $_SERVER['REQUEST_URI'] = '/api/users';
+
+        $request = new Request();
+
+        $this->assertEquals('', $request->query());
+    }
+
+    public function testRequestUrlWithComplexPath()
+    {
+        $_SERVER['REQUEST_URI'] = '/api/v1/users/123/posts?filter=active';
+
+        $request = new Request();
+
+        $this->assertEquals('/api/v1/users/123/posts', $request->requestUrl());
+        $this->assertEquals('filter=active', $request->query());
+    }
+}
