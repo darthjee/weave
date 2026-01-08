187919b73dec9f864c2e9bbd929257776d9b0f37
darthjee <darthjee@gmail.com>
2026-01-08 17:30:51 -0300

    Extract adding targets

--- proxy/source/lib/Configuration.php
+++ proxy/source/lib/Configuration.php
@@ -4,22 +4,32 @@ namespace Tent;
 
 class Configuration
 {
+    private static $targets = [];
+
+    public static function addTarget($target)
+    {
+        self::$targets[] = $target;
+    }
+
     public static function getTargets()
     {
-        return [
-            new ProxyTarget(
-                new ProxyRequestHandler(new Server('http://frontend:8080')),
-                [
-                    new RequestMatcher('GET', '/', 'exact'),
-                    new RequestMatcher('GET', '/assets/images/', 'begins_with'),
-                    new RequestMatcher('GET', '/assets/js/', 'begins_with'),
-                    new RequestMatcher('GET', '/assets/css/', 'begins_with'),
-                    new RequestMatcher('GET', '/@vite/', 'begins_with'),
-                    new RequestMatcher('GET', '/node_modules/', 'begins_with'),
-                    new RequestMatcher('GET', '/@react-refresh', 'exact')
-                ]
-            ),
-            new ProxyTarget(new MissingRequestHandler())
-        ];
+        return self::$targets;
     }
 }
+
+Configuration::addTarget(
+    new ProxyTarget(
+        new ProxyRequestHandler(new Server('http://frontend:8080')),
+        [
+            new RequestMatcher('GET', '/', 'exact'),
+            new RequestMatcher('GET', '/assets/images/', 'begins_with'),
+            new RequestMatcher('GET', '/assets/js/', 'begins_with'),
+            new RequestMatcher('GET', '/assets/css/', 'begins_with'),
+            new RequestMatcher('GET', '/@vite/', 'begins_with'),
+            new RequestMatcher('GET', '/node_modules/', 'begins_with'),
+            new RequestMatcher('GET', '/@react-refresh', 'exact')
+        ]
+    )
+);
+
+Configuration::addTarget(new ProxyTarget(new MissingRequestHandler()));
