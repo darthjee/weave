f092ecbee2675af2325bbc69d668223dd84b64a2
darthjee <darthjee@gmail.com>
2026-01-08 20:11:05 -0300

    Lint fix

--- proxy/tests/unit/lib/models/FolderLocationTest.php
+++ proxy/tests/unit/lib/models/FolderLocationTest.php
@@ -12,14 +12,14 @@ class FolderLocationTest extends TestCase
     public function testBasePathReturnsTheBasePath()
     {
         $location = new FolderLocation('/var/www/html');
-        
+
         $this->assertEquals('/var/www/html', $location->basePath());
     }
 
     public function testConstructorAcceptsBasePath()
     {
         $location = new FolderLocation('/path/to/files');
-        
+
         $this->assertEquals('/path/to/files', $location->basePath());
     }
 }
