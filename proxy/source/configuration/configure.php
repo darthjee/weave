83fe6ad14b39dc3ab8aa2e00ef70d32591bdba8c
darthjee <darthjee@gmail.com>
2026-01-08 20:35:58 -0300

    Serve images as static

--- proxy/source/configuration/configure.php
+++ proxy/source/configuration/configure.php
@@ -3,15 +3,25 @@
 use Tent\Configuration;
 use Tent\Rule;
 use Tent\ProxyRequestHandler;
+use Tent\StaticFileHandler;
 use Tent\Server;
+use Tent\FolderLocation;
 use Tent\RequestMatcher;
+
+Configuration::addRule(
+    new Rule(
+        new StaticFileHandler(new FolderLocation('/var/www/html/static')),
+        [
+            new RequestMatcher('GET', '/assets/images/', 'begins_with'),
+        ]
+    )
+);
 
 Configuration::addRule(
     new Rule(
         new ProxyRequestHandler(new Server('http://frontend:8080')),
         [
             new RequestMatcher('GET', '/', 'exact'),
-            new RequestMatcher('GET', '/assets/images/', 'begins_with'),
             new RequestMatcher('GET', '/assets/js/', 'begins_with'),
             new RequestMatcher('GET', '/assets/css/', 'begins_with'),
             new RequestMatcher('GET', '/@vite/', 'begins_with'),
