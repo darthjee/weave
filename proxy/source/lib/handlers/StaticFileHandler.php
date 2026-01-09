a7b2951b1a0649d76479ad87842133f2deed8d2c
darthjee <darthjee@gmail.com>
2026-01-08 21:14:22 -0300

    Lint fix

--- proxy/source/lib/handlers/StaticFileHandler.php
+++ proxy/source/lib/handlers/StaticFileHandler.php
@@ -36,11 +36,11 @@ class StaticFileHandler implements RequestHandler
     private function getContentType($filePath)
     {
         $mimeType = mime_content_type($filePath);
-        
+
         // If mime_content_type returns a generic type, use extension-based detection
         if ($mimeType === 'text/plain' || $mimeType === 'application/octet-stream') {
             $extension = pathinfo($filePath, PATHINFO_EXTENSION);
-            
+
             $extensionMap = [
                 'css' => 'text/css',
                 'js' => 'application/javascript',
@@ -50,12 +50,12 @@ class StaticFileHandler implements RequestHandler
                 'svg' => 'image/svg+xml',
                 'xml' => 'application/xml',
             ];
-            
+
             if (isset($extensionMap[$extension])) {
                 return $extensionMap[$extension];
             }
         }
-        
+
         return $mimeType;
     }
 }
