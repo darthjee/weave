b536c251411f23df929184b144bca72ce33d7805
darthjee <darthjee@gmail.com>
2026-01-16 12:56:59 -0300

    Add missing file

---
+++ proxy/source/lib/exceptions/FileNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tent\Exceptions;
+
+class FileNotFoundException extends \Exception
+{
+}
---
+++ proxy/source/lib/exceptions/InvalidFilePathException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tent\Exceptions;
+
+class InvalidFilePathException extends \Exception
+{
+}
