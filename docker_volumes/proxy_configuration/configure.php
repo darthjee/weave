f96869cbeea4709f5c8e26397737b3f586c49111
darthjee <darthjee@gmail.com>
2026-01-16 17:36:10 -0300

    Fix configuration

--- docker_volumes/proxy_configuration/configure.php
+++ docker_volumes/proxy_configuration/configure.php
@@ -9,29 +9,46 @@ use Tent\Models\Server;
 use Tent\Models\FolderLocation;
 use Tent\Models\RequestMatcher;
 
-Configuration::addRule(
-    new Rule(
-        new StaticFileHandler(new FolderLocation('./static')),
-        [
-            new RequestMatcher('GET', '/index.html', 'exact'),
-            new RequestMatcher('GET', '/assets', 'begins_with'),
-        ]
-    )
-);
-Configuration::addRule(
-    new Rule(
-        new FixedFileHandler('./static/index.html'),
-        [
-            new RequestMatcher('GET', '/', 'exact'),
-        ]
-    )
-);
+if (getenv('FRONTEND_DEV_MODE') === 'true') {
+    Configuration::addRule(
+        new Rule(
+            new ProxyRequestHandler(new Server('http://frontend:8080')),
+            [
+                new RequestMatcher('GET', '/', 'exact'),
+                new RequestMatcher('GET', '/assets/js/', 'begins_with'),
+                new RequestMatcher('GET', '/assets/css/', 'begins_with'),
+                new RequestMatcher('GET', '/@vite/', 'begins_with'),
+                new RequestMatcher('GET', '/node_modules/', 'begins_with'),
+                new RequestMatcher('GET', '/@react-refresh', 'exact')
+            ]
+        )
+    );
+} else {
+    Configuration::addRule(
+        new Rule(
+            new StaticFileHandler(new FolderLocation('/var/www/html/static')),
+            [
+                new RequestMatcher('GET', '/index.html', 'exact'),
+                new RequestMatcher('GET', '/assets', 'begins_with'),
+            ]
+        )
+    );
+    Configuration::addRule(
+        new Rule(
+            new FixedFileHandler('/var/www/html/static/index.html'),
+            [
+                new RequestMatcher('GET', '/', 'exact'),
+            ]
+        )
+    );
+}
 
 Configuration::addRule(
     new Rule(
-        new ProxyRequestHandler(new Server('https://weave.tamanduati.tech/')),
+        new StaticFileHandler(new FolderLocation('/var/www/html/static')),
         [
-            new RequestMatcher('GET', '/api/', 'begins_with')
+            new RequestMatcher('GET', '/assets/images/', 'begins_with'),
+            new RequestMatcher('GET', '/assets/admin/', 'begins_with'),
         ]
     )
 );
